b548d7458d51c5d08c33c3d9ed36885ac0fae8f8
Ruxin <ruxin@peaksandpies.com>
2024-10-22 16:16:55 +0200

    add tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+# Define the Azure provider
+provider "azurerm" {
+  features {}
+}
+
+# Resource group definition
+resource "azurerm_resource_group" "example_rg" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+# Storage account definition
+resource "azurerm_storage_account" "example_sa" {
+  name                     = "examplestorageacct"
+  resource_group_name      = azurerm_resource_group.example_rg.name
+  location                 = azurerm_resource_group.example_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"  # Locally redundant storage
+}
+
