bd96bc3c311b19e9dea37cea69af1ab6c9373a94
Ruxin <ruxin@peaksandpies.com>
2024-10-25 09:03:55 +0200

    remove example

--- main.tf
+++ main.tf
@@ -10,14 +10,14 @@ resource "azurerm_resource_group" "example_rg" {
 }
 
 # Storage account definition
-resource "azurerm_storage_account" "example_sa" {
-  name                     = "examplestorageacct"
-  resource_group_name      = azurerm_resource_group.example_rg.name
-  location                 = azurerm_resource_group.example_rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"  # Locally redundant storage
-  identity {
-    type = "SystemAssigned"
-  }
-}
+# resource "azurerm_storage_account" "example_sa" {
+#   name                     = "examplestorageacct"
+#   resource_group_name      = azurerm_resource_group.example_rg.name
+#   location                 = azurerm_resource_group.example_rg.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"  # Locally redundant storage
+#   identity {
+#     type = "SystemAssigned"
+#   }
+# }
 
